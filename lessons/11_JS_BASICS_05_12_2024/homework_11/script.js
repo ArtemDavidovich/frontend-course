9a737493ed460d061904891c68a78f6c6397b2a8
Artem Davidovich <chumazek@gmail.com>
2024-12-06 09:41:28 +0100

    fix

--- lessons/11_JS_BASICS_05_12_2024/homework_11/script.js
+++ lessons/11_JS_BASICS_05_12_2024/homework_11/script.js
@@ -1,13 +1,20 @@
 // Создайте переменную myNumber и присвойте ей результат выражения: деление числа 100 на остаток от деления 25 на 3. Выведите значение myNumber в консоль.
-let myNumber = 100 / (25 % 3)
+let number = 100
+let myNumber = number / (25 % 3)
 console.log(myNumber)
 
 // Создайте переменную myString и присвойте ей значение, которое является результатом сложения следующих строк: "Hello, ", "world", "!", и числа 2024. Выведите значение myString в консоль.
-let myString = "Hello, " + "world" + "!" + 2024
+let string1 = "Hello, "
+let string2 = "world"
+let string3 = "!"
+number = 2024
+let myString = string1 + string2 + string3 + number
 console.log(myString)
 
 // Создайте переменную isHuman и присвойте ей результат сравнения числа 42 со строкой 42. Результатом должно стать true. Подумайте какой оператор сравнения вы будете использовать.
-let isHuman = 42 == '42'
+number = 42
+string = '42'
+let isHuman = number == string
 console.log(isHuman)
 
 // Создайте переменную myNumber и присвойте ей результат преобразования строки "123smth" в число. Выведите значение myNumber в консоль.
