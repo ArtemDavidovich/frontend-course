bf5b56dafbeaa963873a236e7809541390b6e981
Artem Davidovich <chumazek@gmail.com>
2024-12-09 11:07:42 +0100

    fix hw12

--- lessons/12_ARRAYS_06_12_2024/homework_12/script.js
+++ lessons/12_ARRAYS_06_12_2024/homework_12/script.js
@@ -1,14 +1,20 @@
 // Задание 1
 // Создайте массив с элементами: "Мария", "Алексей", "Елена", "Дмитрий". Создайте массив с возрастами: 22, 31, 45, 53. Создайте новый массив, заполнив его элементами в формате "имя возраст лет/годов".
 
-let names = ["Мария", "Алексей", "Елена", "Дмитрий"]
+const names = ["Мария", "Алексей", "Елена", "Дмитрий"]
 
-let ages = [22, 31, 45, 53]
+// поверхностное копирование
+// let namesCopy = names
+
+// глубокое копирование
+
+const ages = [22, 31, 45, 53]
 
 let people = []
 
-for(i = 0, j = 0, k = 0; i < names.length || j < ages.length; i++, j++, k++) {
-    people[k] = names[i] + " " + ages[j]
+for(i = 0; i < names.length; i++) {
+    people.push(`${names[i]} ${ages[i]} лет/годов`) // шаблонная строка
+    // people.push(names[i] + " " + ages[i] + " лет/годов") //конкатенация
 }
 
 console.log("--------Задание 1--------")
@@ -25,11 +31,9 @@ console.log(reversePeople)
 // Задание 3
 // Создайте пустой массив countries. Добавьте в массив следующие страны: "Франция", "Германия", "Италия". Удалите последний элемент из массива и сохраните значение в переменной. Добавьте его в начало массива. Выведите countries в консоль.
 
-let countries = []
+const countries = []
 
-countries.push("Франция")
-countries.push("Германия")
-countries.push("Италия")
+countries.push("Франция", "Германия", "Италия")
 
 let lastElementCountries = countries.pop()
 
@@ -45,19 +49,24 @@ console.log(countries)
 // Создайте новый массив, где каждый элемент будет равен квадрату элемента из исходного массива.
 // Выведите оба массива (исходный и новый) в консоль.
 
-let numbers = [1, 2, 3, 4, 5]
-
-for (let i = 0; i < numbers.length; i++){    
-    numbers[i] = numbers[i] * 2    
-}
-
 console.log("--------Задание 4--------")
-console.log("Исходный массив:", numbers)
 
+let numbers = [1, 2, 3, 4, 5]
 let squares = []
 
-for(i = 0, j = 0; i < numbers.length; i++, j++){
-    squares[j] = numbers[i] ** 2
+for (const i in numbers) {
+    console.log(numbers[i] * 2)
+    squares.push(numbers[i] * numbers [i])
 }
+
+// for (let i = 0; i < numbers.length; i++){    
+//    console.log(numbers[i] * 2)    
+// }
+
+// for(i = 0; i < numbers.length; i++){
+//     squares[i] = numbers[i] ** 2
+// }
+
+console.log("Исходный массив:", numbers)
 
 console.log("Новый массив:", squares)
